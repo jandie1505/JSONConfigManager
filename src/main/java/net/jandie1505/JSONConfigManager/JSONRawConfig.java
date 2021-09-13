6d13cfb535f97f0bde4001f2850e5da388c74940
jandie1505 <jande1234567890@gmail.com>
2021-09-13 22:03:01 +0200

    v2 - added JSONRawConfig

---
+++ src/main/java/net/jandie1505/JSONConfigManager/JSONRawConfig.java
@@ -0,0 +1,246 @@
+package net.jandie1505.JSONConfigManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Map;
+
+public class JSONRawConfig {
+    private boolean verboseLogging;
+    private String jsonString;
+
+    /**
+     * Creates JSONConfig by File Object
+     * @throws IOException
+     */
+    public JSONRawConfig() throws IOException {
+        setVerbose(false);
+        init();
+    }
+
+    /**
+     * Returns the raw JSON String
+     * @return JSON String (String)
+     */
+    public String getJSONString(){
+        return jsonString;
+    }
+
+    /**
+     * Get the JSONObject of the JSONConfig
+     * @return JSONObject
+     * @throws IOException
+     */
+    public JSONObject getJSONObject() throws IOException {
+        return load();
+    }
+
+    /**
+     * Override the JSONObject.
+     * Not recommended to use this.
+     * @param jsonObject
+     * @throws IOException
+     * @deprecated Use overrideJSONObject instead
+     */
+    @Deprecated
+    public void setJSONObject(JSONObject jsonObject) throws IOException {
+        save(jsonObject);
+    }
+    /**
+     * Override the JSONObject.
+     * Not recommended to use this.
+     * @param jsonObject
+     * @throws IOException
+     */
+    public void overrideJSONObject(JSONObject jsonObject) throws IOException {
+        save(jsonObject);
+    }
+
+    public void put(String key, Object value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, int value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, boolean value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, String value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, double value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, long value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, float value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, Map value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, Collection value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, JSONObject value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+    public void put(String key, JSONArray value) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.put(key, value);
+        save(jsonObject);
+    }
+
+    public Object get(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.get(key);
+    }
+    public String getString(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getString(key);
+    }
+    public int getInt(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getInt(key);
+    }
+    public boolean getBoolean(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getBoolean(key);
+    }
+    public float getFloat(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getFloat(key);
+    }
+    public double getDouble(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getDouble(key);
+    }
+    public JSONArray getJSONArray(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getJSONArray(key);
+    }
+    public BigDecimal getBigDecimal(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getBigDecimal(key);
+    }
+    public BigInteger getBigInteger(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getBigInteger(key);
+    }
+    public JSONObject getJSONObject(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.getJSONObject(key);
+    }
+
+    public boolean has(String key) throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.has(key);
+    }
+
+    /**
+     * Removes a key from the JSONConfig.
+     * @param key
+     * @throws IOException
+     */
+    public void remove(String key) throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.remove(key);
+        save(jsonObject);
+    }
+
+    /**
+     * Returns the JSONObject as a String.
+     * @return String
+     */
+    @Override
+    public String toString(){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
+
+    /**
+     * Returns an Array with the keys in it (for for each loops).
+     * @return Object[] keyArray
+     * @throws IOException
+     */
+    public Object[] getKeyArray() throws IOException {
+        JSONObject jsonObject = load();
+        return jsonObject.keySet().toArray();
+    }
+
+    /**
+     * Clears the JSONConfig.
+     * @throws IOException
+     */
+    public void clear() throws IOException {
+        JSONObject jsonObject = load();
+        jsonObject.clear();
+        save(jsonObject);
+    }
+
+    /**
+     * Enable/Disable verbose logging.
+     * @param verbose
+     */
+    public void setVerbose(boolean verbose) {
+        verboseLogging = verbose;
+    }
+
+    private void init() throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonString = jsonObject.toString();
+        load();
+        verbose("Initialized JSONConfig");
+    }
+
+    private JSONObject load() throws IOException {
+        JSONObject jsonObject = new JSONObject(jsonString);
+        verbose("Loaded JSON String");
+        return jsonObject;
+    }
+
+    private void save(JSONObject jsonObject) throws IOException {
+        jsonString = jsonObject.toString();
+        verbose("Saved JSON String");
+    }
+
+    private void verbose(String message) {
+        if(verboseLogging){
+            System.out.println("[JSONConfig] " + message);
+        }
+    }
+
+    private static void message(String message) {
+        System.out.println("[JSONConfig] " + message);
+    }
+}
