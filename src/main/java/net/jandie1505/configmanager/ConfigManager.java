209c42172e832ff6ddcb2348fbe08e4a8f679d4c
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-05-30 20:49:36 +0200

    uploaded to github

---
+++ src/main/java/net/jandie1505/configmanager/ConfigManager.java
@@ -0,0 +1,71 @@
+package net.jandie1505.configmanager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+
+public class ConfigManager {
+    private final File configFile;
+    private final boolean overrideDefaults;
+    private JSONObject config;
+
+    public ConfigManager(JSONObject defaultConfig, boolean overrideDefaults, File dataFolder, String filename) {
+        this.configFile = new File(dataFolder, filename);
+        this.config = new JSONObject(defaultConfig.toString());
+        this.overrideDefaults = overrideDefaults;
+    }
+
+    private JSONObject loadConfig() throws IOException, JSONException {
+        BufferedReader br = new BufferedReader(new FileReader(this.configFile));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
+        return new JSONObject(sb.toString());
+    }
+
+    private void writeConfig() throws IOException {
+        FileWriter writer = new FileWriter(this.configFile);
+        writer.write(this.config.toString(4));
+        writer.flush();
+        writer.close();
+    }
+
+    public void reloadConfig() {
+        try {
+            if (!this.configFile.exists()) {
+                this.configFile.getParentFile().mkdirs();
+                this.configFile.createNewFile();
+                this.writeConfig();
+            }
+
+            JSONObject loadedConfig = this.loadConfig();
+
+            if (this.overrideDefaults) {
+
+                this.config = new JSONObject(loadedConfig.toString());
+
+            } else {
+
+                if (loadedConfig.optBoolean("recreateConfig", false)) {
+                    this.writeConfig();
+                } else {
+                    for (String key : loadedConfig.keySet()) {
+                        this.config.put(key, loadedConfig.get(key));
+                    }
+                }
+
+            }
+        } catch (IOException | JSONException e) {
+            System.err.println("Error loading config, using defaults");
+        }
+    }
+
+    public JSONObject getConfig() {
+        return new JSONObject(this.config.toString());
+    }
+}
